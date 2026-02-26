6abe4074d5ecc49f59204a5fca6373570befd3b5
User <user@example.com>
2026-02-26 18:24:56 -0300

    feat: add favicon SVG for browser tab

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="es" data-theme="dark">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       </head>
